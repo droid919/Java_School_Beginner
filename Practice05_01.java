a340688943544371c87c8e0a2332f9999abf6315
droid919 <89201168+droid919@users.noreply.github.com>
2024-11-20 20:48:21 +0900

    Add files via upload

---
+++ Practice05_01.java
@@ -0,0 +1,30 @@
+public class Practice05_01 {
+    public static void main(String[] args) {
+        // 1
+        String[] arr_01 ={"リンゴ","バナナ","メロン","マスカット"};
+        
+        System.out.println(arr_01[0]);
+        System.out.println(arr_01[1]);
+        System.out.println(arr_01[2]);
+        System.out.println(arr_01[3]);
+
+        //2
+        int[] arr_02 ={10,20,100,3};
+
+        System.out.println(arr_02[0]);
+        System.out.println(arr_02[1]);
+        System.out.println(arr_02[2]);
+        System.out.println(arr_02[3]);
+
+        //3
+        String[][] arr_03 ={{"ポチ","ジョン"},{"タマ","もちもる"},{"ペンペン","ペンギン丸"}};
+
+        System.out.println(arr_03[0][0]);
+        System.out.println(arr_03[0][1]);
+        System.out.println(arr_03[1][0]);
+        System.out.println(arr_03[1][1]);
+        System.out.println(arr_03[2][0]);
+        System.out.println(arr_03[2][1]);
+
+    }
+}
