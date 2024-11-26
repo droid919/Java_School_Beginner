979e635068be6f118ca58d156e94996fb837e1f8
droid919 <89201168+droid919@users.noreply.github.com>
2024-11-26 12:44:44 +0900

    Update Practice06_04.java

--- Lesson_06_演算子/Practice06_04.java
+++ Lesson_06_演算子/Practice06_04.java
@@ -1,11 +1,11 @@
 public class Practice06_04 {
     public static void main(String[] args) { 
         int a = 3;
-        Boolean c = a == 2 && a < 3;
+        Boolean c = (a != 2 && a < 3);
         System.out.println("a は 2でない かつ、3未満 の結果は " + c + " です");
 
         int b = 5;
-        Boolean d = b == 1 || b >= 5;
+        Boolean d = (b == 1 || b >= 5);
         System.out.println("b は 1である または、5以上 の結果は " + d + " です");
     }
 }
