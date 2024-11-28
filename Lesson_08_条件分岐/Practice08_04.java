1743cfcd29d243135d91a5adb4d1d1772a0eb2e4
droid919 <89201168+droid919@users.noreply.github.com>
2024-11-28 19:01:05 +0900

    Add files via upload

--- Lesson_08_条件分岐/Practice08_04.java
+++ Lesson_08_条件分岐/Practice08_04.java
@@ -10,7 +10,7 @@ public class Practice08_04 {
 
         if (color == 1) {
             System.out.println("今日のラッキーカラーは ピンク です");
-        } else if(color >= 2 && color <= 4) {
+        } else if(color == 2 || color == 3 || color == 4) {
             System.out.println("今日のラッキーカラーは レッド です");
         } else {
             System.out.println("今日のラッキーカラーは ゴールド です");
