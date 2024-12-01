046cf06f5ffa7366c8bb82f8c3ac88eb5d1773d9
droid919 <89201168+droid919@users.noreply.github.com>
2024-12-01 14:55:25 +0900

    Add files via upload

--- 総合問題/ComprehensiveBeginner01_01.java
+++ 総合問題/ComprehensiveBeginner01_01.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ComprehensiveBeginner01_01 {
+    
     public static void main(String[] args) {
-        int array[] = {1, 50, 10};
+        Scanner sc = new Scanner(System.in, "Shift-JIS");
+        ArrayList<Integer> arrint = new ArrayList<>();
+        
+        System.out.print("整数を入力してください。複数の整数があれば、数字と数字の間に「,」を入れてください。(例:12,5,60)<");
+        String inputStr = sc.nextLine();
+        
+        String[] s = inputStr.split(",");
+        
+        for (int i = 0; i < s.length; i++){
+            arrint.add(Integer.parseInt(s[i]));
+        }
+
         for (int i = 0; i < 3; i++){
-            System.out.println(i + 1 + "番目の要素の値は" + array[i] + "で、2倍にすると" + array[i] * 2 + "になり、3 で割ったあまりは" + array[i] % 3 + "です");
+            System.out.println(
+                i + 1 + "番目の要素の値は" + arrint.get(i) + 
+            "で、2倍にすると" + arrint.get(i) * 2 + 
+            "になり、3 で割ったあまりは" +
+             arrint.get(i) % 3 + "です");
         }
     }
 }
